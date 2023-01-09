9b61a6952a3a892df394074509709b3863ce47f6
rizatsk <rzsakmir@gmail.com>
2023-01-09 22:34:41 +0700

    login logout 2 table

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => ['auth:useradmin,pelanggan']], function () {
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+Route::post('/authentication', [AuthenticationController::class, 'authentication'])->middleware('guest');
+Route::get('/', [AuthenticationController::class, 'index'])->middleware('guest')->name('authentication');
